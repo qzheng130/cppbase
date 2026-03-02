0c9c564a67102cbeae0be91efd36f6277aa07dd2
qzheng130 <zhengqiang130@foxmail.com>
2026-03-02 17:56:20 +0800

    cmake.

--- build.sh
+++ build.sh
@@ -26,7 +26,6 @@ mkdir -p $BUILD_DIR/$BUILD_TYPE \
   && cmake \
            -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
            -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
-           -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
            $SOURCE_DIR \
   && make $* 
 
